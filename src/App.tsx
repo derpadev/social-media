44c0b6c2211058d6629731bbeee662b8e3d1b98d
Devin Hua <devinhua004@gmail.com>
2025-11-11 15:47:51 -0800

    created navbar, set up OAuth w/ GitHub and Supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router"
-import { Home } from "./components/Home"
+import { Home } from "./pages/Home"
+import { Navbar } from "./components/Navbar"
 
 function App() {
   return (
-  <div>
-    <div>
+  <div className="bg-black min-h-screen text-gray-100 transition-opacity duration-700 pt-20">
+    <Navbar />
+    <div className="px-4 py-6 container mx-auto">
       <Routes>
         <Route path="/" element={<Home />}/>
       </Routes>
